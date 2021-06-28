60ed1c280710843912f78128e2cfb2668f613ad0
Mandor99 <mahmoudmandor65@gmail.com>
2021-06-28 02:12:37 +0200

    handle errors in createPost

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,11 +1,12 @@
 import {useMutation} from '@apollo/client'
-import React, {useState} from 'react'
-import {Form, Button} from 'semantic-ui-react'
+import React, {useState, useEffect} from 'react'
+import {Form, Button, Message} from 'semantic-ui-react'
 import {createPostMutation, getPosts} from '../graphQl/Query'
 
 function PostForm() {
     const [post, setPost] = useState('')
-    const [createPost] = useMutation(createPostMutation, {
+    const [err, setErr] = useState({})
+    const [createPost, {error}] = useMutation(createPostMutation, {
         update(cache, {data: {createPost}}) {
             const data = cache.readQuery({query: getPosts})
             cache.writeQuery({
@@ -16,20 +17,31 @@ function PostForm() {
             })
             console.log(data);
         },
+        onError({graphQLErrors}) {
+            setErr(graphQLErrors[0])
+            console.log(err);
+        }
         // refetchQueries: [{query: getPosts}] ==>> it's true but i would to use cache update
     })
+    // useEffect(() => {
+    //     return createPost({variables: {body: post}})
+    // }, [createPost, post])
     const handleInput = (setter) => (e) => setter(e.target.value)
     const handleSubmit = (e) => {
         e.preventDefault()
         createPost({variables: {body: post}})
+        setPost('')
     }
     return (
         <div className='w-60-center'>
             <h3>What's in your mind: </h3>
             <Form onSubmit={handleSubmit}>
-                <Form.Input placeholder='I think in ...' type='text' value={post} onChange={handleInput(setPost)}/>
+                <Form.Input placeholder='I think in ...' type='text' value={post} onChange={handleInput(setPost)} error={error ? true : false}/>
                 <Button type='submit' color='teal'> Share Post </Button>
             </Form>
+            {
+                error && (<Message error header={err?.message?.slice(15)} />)
+            }
         </div>
     )
 }
